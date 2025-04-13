9be60bf7433d270042e683ec9e10a3485b9b85c2
MoisesAzevedo <moisesdev2022@gmail.com>
2025-04-13 19:05:37 -0300

    fix(/images): add public folder

---
+++ public/img/icons/burg-menu.js
@@ -0,0 +1,6 @@
+export const burg_menu_svg = `<svg width="14" height="12" viewBox="0 0 14 12" fill="none" xmlns="http://www.w3.org/2000/svg">
+  <line x1="1" y1="6" x2="13" y2="6" stroke-width="2" stroke-linecap="round" class="linha"/>
+  <line x1="1" y1="11" x2="13" y2="11" stroke-width="2" stroke-linecap="round" class="linha"/>
+  <line x1="1" y1="1" x2="13" y2="1" stroke-width="2" stroke-linecap="round" class="linha"/>
+</svg>
+`;
